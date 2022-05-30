e0689db413901b8f1c108fc18bdb4e2ca3e07f7b
Ajayi bolarinwa <ajayibcd@gmail.com>
2022-05-30 02:11:37 -0700

    Added project structure settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CryptoListComponent } from './crypto-list/crypto-list.component';
+import { CoinDetailsComponent } from './coin-details/coin-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CryptoListComponent,
+    CoinDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
